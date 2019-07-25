f8d32c846b3ec5bfc9f4b29511cdffefb8f0a0fe
Martin Connor <martinconnor@Martins-MacBook-Pro-3.local>
2019-07-25 11:26:20 -0400

    Initial commit.

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import DashboardContainer from './components/DashboardContainer';
 
-const App = (props) => (
-  <DashboardContainer example="Coming soon!" />
-);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div>App!</div>
+        <DashboardContainer example="Coming soon!"/>
+      </div>
+    )
+  }
+}
 
 export default App;
